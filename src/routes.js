f5203a1f2844078fb0a58e2ef4dd2d80f2d5b959
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-22 16:56:20 -0200

    'criadorotateste'

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express'
 import app from './app'
 const routes = new Router()
 
-routes.get('/',(req,res)=>{
-    return res.json({ok: true})
+routes.get('/', (req, res) => {
+    return res.json({ ok: true })
 })
 
 export default routes
