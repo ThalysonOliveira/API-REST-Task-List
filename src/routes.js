79aa721ce09976d9cb3eb1329aa26b7f9a5c99d3
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-22 18:30:50 -0200

    'criadorotaSessions'

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ import { Router } from 'express'
 const routes = new Router()
 
 import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
 
 routes.post('/user',UserController.store)
+routes.post('/sessions', SessionController.store)
 
 export default routes
