ea808391678b0c0cca1a2f92d7ed231ab9aa5522
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-23 00:04:27 -0200

    'criadorotadeautenticacaoerotadeupdate'

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
 const routes = new Router()
 
+import authMiddleware from './app/middlewares/auth'
+
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 
 routes.post('/user',UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware)
+routes.put('/users', SessionController.update)
+
+
 
 export default routes
