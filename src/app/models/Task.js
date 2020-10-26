3cdd6e7e9aa874809bbef05c9ac4873a47152956
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-25 23:25:44 -0200

    'identadopretier'

--- src/app/models/Task.js
+++ src/app/models/Task.js
@@ -1,21 +1,23 @@
-import Sequelize, {Model} from 'sequelize'
+import Sequelize, { Model } from 'sequelize';
 
-class Task extends Model{
-    static init(sequelize){
-        super.init(
-            {
-                task: Sequelize.STRING,
-                check: Sequelize.BOOLEAN
-            },
-            {
-                sequelize
-            }
-        )
-        return this
-    }
-    static associate(models){
-        this.belongsTo(models.User, {foreignKey: 'user_id', as: 'user'})
-    }
+class Task extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        task: Sequelize.STRING,
+        check: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+  }
 }
 
-export default Task
+export default Task;
