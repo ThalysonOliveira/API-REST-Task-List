e4d4fc1619739debe58c4ce0d39116fc8a6ce454
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-25 23:07:38 -0200

    'corrigidonomedacoluna''

--- src/database/migrations/20201026003144-create.js
+++ src/database/migrations/20201026003144-create.js
@@ -9,7 +9,7 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true
       },
-      chek: {
+      check: {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
         allowNull: false
