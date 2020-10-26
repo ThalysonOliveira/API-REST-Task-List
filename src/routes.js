88b5297ee2cd105e8e0127bb66bb4d68409860ec
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-25 22:55:09 -0200

    'criadorotadetasks'

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import authMiddleware from './app/middlewares/auth'
 
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
+import TaskController from './app/controllers/TaskController'
 
 routes.post('/user',UserController.store)
 routes.post('/sessions', SessionController.store)
 
 routes.use(authMiddleware)
 routes.put('/users', UserController.update)
+routes.post('tasks', TaskController.store)
 
 
 
