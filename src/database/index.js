6f297cf8af0c913c6379731001cda40ab1812217
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-25 23:27:06 -0200

    'identadopretier'

--- src/database/index.js
+++ src/database/index.js
@@ -1,23 +1,23 @@
-import sequelize, { Sequelize } from 'sequelize'
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
 
-import databaseConfig from '../config/database'
-import User from '../app/models/User'
-import Task from '../app/models/Task'
+import User from '../app/models/User';
+import Task from '../app/models/Task';
 
-const models = [User, Task]
+const models = [User, Task];
 
-class Database{
-    constructor(){
-        this.init()
-    }
+class Database {
+  constructor() {
+    this.init();
+  }
 
-    init(){
-        this.connection = new Sequelize(databaseConfig)
+  init() {
+    this.connection = new Sequelize(databaseConfig);
 
-        models
-        .map(model=> model.init(this.connection))
-        .map(model=> model.associate && model.associate(this.connection.models))
-    }
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
 }
 
-export default new Database
+export default new Database();
