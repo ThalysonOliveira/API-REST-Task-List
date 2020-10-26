9b9754584843ace706c0136b7d1ee001964bbc69
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-25 22:08:57 -0200

    'criadometodoupdatedousuario'

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -9,7 +9,32 @@ class UserController {
         }
 
         const { id, name, email } = await User.create(req.body)
-        return res.json({id, name, email})
+        return res.json({ id, name, email })
+    }
+    async update(req, res) {
+        const { email, oldPassword } = req.body
+
+        const user = await User.findByPk(req.userId)
+
+        if (email !== user.email) {
+            const userExist = await User.findOne({ where: { email } })
+ 
+            if (userExist) {
+                return res.status(400).json({ error: 'Email já cadastrado!' })
+            }
+        }
+
+        if(oldPassword && !(await user.chekedPassword(oldPassword))){
+            return res.status(401).json({error:'Senha incorreta' })
+        }
+
+        const {id, name} = await user.update(req.body)
+
+        return res.json({ 
+            id,
+            name, 
+            email
+          })
     }
 }
 
